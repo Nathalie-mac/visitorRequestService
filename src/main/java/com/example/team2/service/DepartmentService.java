458ca7b182a8ecb9159dc46d5e5e6da8faf05cf6
simeon_mur <simeon.muravyov@gmail.com>
2025-05-08 18:13:05 +0300

    DB test populate

---
+++ src/main/java/com/example/team2/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.example.team2.service;
+
+import com.example.team2.model.Department;
+import com.example.team2.repository.DepartmentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public void save(Department department) {
+        departmentRepository.save(department);
+    }
+
+    public List<Department> getAll() {
+        return departmentRepository.findAll();
+    }
+}
