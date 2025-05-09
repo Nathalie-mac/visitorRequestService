251df063a2f8bbe360474af40a121284de466d12
Liza Antipatrova <Liza.Antipa@gmail.com>
2025-05-09 18:57:12 +0300

    Worker Auth work-work

--- src/main/java/com/example/team2/auth/controller/AuthManagerController.java
+++ src/main/java/com/example/team2/auth/controller/AuthManagerController.java
@@ -1,6 +1,7 @@
 package com.example.team2.auth.controller;
 
 import com.example.team2.auth.services.AuthClientService;
+import com.example.team2.model.StuffRoleType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -8,23 +9,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
-@RequestMapping("/auth")
+@RequestMapping("/auth/manager")
 @RequiredArgsConstructor
-public class AuthClientController {
-
+public class AuthManagerController {
 
     private final AuthClientService authService;
 
+    //TODO: снести все для регистрации после полной отладки
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@RequestHeader("Authorization") String authorizationHeader) {
-        return authService.signUpClient(authorizationHeader);
+        return authService.signUpStuff(authorizationHeader, StuffRoleType.MANAGER);
     }
 
     @PostMapping("/sign-in")
     public ResponseEntity<?> signIn(@RequestHeader("Authorization") String authorizationHeader) {
-        return authService.signInClient(authorizationHeader);
+        return authService.signInStuff(authorizationHeader, StuffRoleType.MANAGER);
     }
 
     @PostMapping("/logout")
