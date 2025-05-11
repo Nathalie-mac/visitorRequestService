ba7612851de4b93a84665a21a44fad7dce594dd9
rubberPlant256 <ihrustal@yandex.ru>
2025-05-11 17:19:54 +0300

    структура клиентских контролеров, аааааааааа

---
+++ src/main/java/com/example/team2/controller/ClientController.java
@@ -0,0 +1,53 @@
+package com.example.team2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/client")
+public class ClientController {
+
+    // Отображение страницы с заявками (главная страница web_request_table)
+    @GetMapping
+    public String showRequestsPage(Model model) {
+        // Здесь должна быть логика получения заявок из базы данных
+
+        return "web_request_table";
+    }
+
+    // Открытие страницы с формой создания заявки (главная страница web_request_table)
+    @GetMapping("/type")
+    public String showRequestForm(Model model) {
+
+        return "web_visit_type";
+    }
+
+    // Открытие страницы создания заявки для личного посещения (страница выбора типа web_visit_type)
+    @GetMapping("/personal-visit")
+    public String personalVisit() {
+        return "personal_visit";
+    }
+
+    // Открытие страницы создания заявки для группового посещения (страница выбора типа web_visit_type)
+    @GetMapping("/group-visit")
+    public String groupVisit() {
+        return "group_visit";
+    }
+
+    // Сохранение данных с формы в бд (страница личной заявки web_request_one и групповая web_request_many)
+    @PostMapping("/new-request")
+    public String handleVisitSubmission(){
+        // 1. Обработка данных формы (web_request_one)
+        // 2. Обработка загруженных файлов:
+        //    - photo (фото посетителя)
+        //    - documents (дополнительные документы)
+            // Сохранение данных и файлов
+            // visitService.saveVisit(appointmentRequest, photo, documents);
+
+        return "redirect:/web_request_table";
+    }
+
+}
