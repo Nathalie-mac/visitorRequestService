8e0cb65a078111e4c7a3cc3f0da6a8f97c316e75
nathalie_mac <natulka0506@gmail.com>
2025-05-12 12:58:04 +0300

    упд правильное заполнение базы (1 инд + 1 групповая)

--- src/main/java/com/example/team2/test/TestDataFiller.java
+++ src/main/java/com/example/team2/test/TestDataFiller.java
@@ -84,8 +84,8 @@ public class TestDataFiller {
         person.setPhoto(1);
         person.setBlackList(false);
         person.setRequest(request);
-        person.setRequest(request2);
         personService.save(person);
+
 
         Person person2 = new Person();
         person2.setFirstName("Имя2");
@@ -101,7 +101,87 @@ public class TestDataFiller {
         person2.setPhoto(2);
         person2.setBlackList(false);
         person2.setRequest(request2);
-        personService.save(person);
+        personService.save(person2);
+
+        Person person3 = new Person();
+        person3.setFirstName("Алексей");
+        person3.setMiddleName("Сергеевич");
+        person3.setLastName("Иванов");
+        person3.setPhone("+7(912)345-67-89");
+        person3.setEmail("alex.ivanov@gmail.com");
+        person3.setNote("Клиент VIP");
+        person3.setOrganization("ООО Рога и Копыта");
+        person3.setBirthDate(LocalDate.parse("1985-07-15"));
+        person3.setPassportSery("4501");
+        person3.setPassportNumber("987654");
+        person3.setPhoto(3);
+        person3.setBlackList(false);
+        person3.setRequest(request2);
+        personService.save(person3);
+
+        Person person4 = new Person();
+        person4.setFirstName("Екатерина");
+        person4.setMiddleName("Викторовна");
+        person4.setLastName("Смирнова");
+        person4.setPhone("+7(923)456-78-90");
+        person4.setEmail("k.smirnova@mail.ru");
+        person4.setNote("Постоянный клиент");
+        person4.setOrganization("ИП Смирнова");
+        person4.setBirthDate(LocalDate.parse("1990-03-22"));
+        person4.setPassportSery("5002");
+        person4.setPassportNumber("123456");
+        person4.setPhoto(4);
+        person4.setBlackList(false);
+        person4.setRequest(request2);
+        personService.save(person4);
+
+        Person person5 = new Person();
+        person5.setFirstName("Дмитрий");
+        person5.setMiddleName("Анатольевич");
+        person5.setLastName("Петров");
+        person5.setPhone("+7(934)567-89-01");
+        person5.setEmail("dmitry.petrov@yandex.ru");
+        person5.setNote("Новый клиент");
+        person5.setOrganization("ЗАО ТехноПром");
+        person5.setBirthDate(LocalDate.parse("1978-11-30"));
+        person5.setPassportSery("6003");
+        person5.setPassportNumber("654321");
+        person5.setPhoto(5);
+        person5.setBlackList(false);
+        person5.setRequest(request2);
+        personService.save(person5);
+
+        Person person6 = new Person();
+        person6.setFirstName("Ольга");
+        person6.setMiddleName("Игоревна");
+        person6.setLastName("Кузнецова");
+        person6.setPhone("+7(945)678-90-12");
+        person6.setEmail("olga.kuznetsova@gmail.com");
+        person6.setNote("Частый заказчик");
+        person6.setOrganization("ООО СтройГарант");
+        person6.setBirthDate(LocalDate.parse("1982-05-18"));
+        person6.setPassportSery("7004");
+        person6.setPassportNumber("789012");
+        person6.setPhoto(6);
+        person6.setBlackList(false);
+        person6.setRequest(request2);
+        personService.save(person6);
+
+        Person person7 = new Person();
+        person7.setFirstName("Артем");
+        person7.setMiddleName("Валерьевич");
+        person7.setLastName("Соколов");
+        person7.setPhone("+7(956)789-01-23");
+        person7.setEmail("artem.sokolov@mail.ru");
+        person7.setNote("Корпоративный клиент");
+        person7.setOrganization("АО ГазНефть");
+        person7.setBirthDate(LocalDate.parse("1995-09-10"));
+        person7.setPassportSery("8005");
+        person7.setPassportNumber("890123");
+        person7.setPhoto(7);
+        person7.setBlackList(false);
+        person7.setRequest(request2);
+        personService.save(person7);
 
         //PDF паспорта
         PassportData passportData = new PassportData();
